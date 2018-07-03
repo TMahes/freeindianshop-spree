7329a34ec2ca4ed2de701e47e175bf0ff4ecc4bd
Maheswaran <t.mahesmca@gmail.com>
2018-07-03 05:51:04 +0530

    multi vendor fix 2

---
+++ app/controllers/spree/admin/supplier_bank_accounts_controller.rb
@@ -0,0 +1,23 @@
+class Spree::Admin::SupplierBankAccountsController < Spree::Admin::ResourceController
+
+  create.before :set_supplier
+
+  def new
+    @object = @supplier.bank_accounts.build
+  end
+
+  private
+
+    def load_supplier
+      @supplier = Spree::Supplier.friendly.find(params[:supplier_id])
+    end
+
+    def location_after_save
+      spree.edit_admin_supplier_path(@supplier)
+    end
+
+    def set_supplier
+      @object.supplier = @supplier
+    end
+
+end

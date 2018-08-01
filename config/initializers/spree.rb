2555da92697f2b8b7af4e72f5e3cc15c53dff620
Maheswaran <t.mahesmca@gmail.com>
2018-08-01 16:38:05 +0530

    minor change

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -18,5 +18,3 @@ end
 
 Spree.user_class = "Spree::User"
 Spree::PermittedAttributes.user_attributes.push :first_name, :last_name, :dob, :city, :state, :zip_code, :address, :seller,:shop_name,:phone
-
-end

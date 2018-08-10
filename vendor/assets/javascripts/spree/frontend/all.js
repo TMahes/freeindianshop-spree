c3d80e730e417ac0826b607f0dfda286e5bbf324
Maheswaran <t.mahesmca@gmail.com>
2018-08-10 07:42:36 +0530

    fron end changes

--- vendor/assets/javascripts/spree/frontend/all.js
+++ vendor/assets/javascripts/spree/frontend/all.js
@@ -12,4 +12,37 @@
 //= require_tree .
 //= require spree/frontend/spree_auth
 //= require spree/frontend/spree_drop_ship
-//= require spree/frontend/spree_themes
+//= require spree/frontend/slick.min
+
+$(document).on('ready', function() {
+
+  // Home Page Slider
+  $(".regular").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  });
+
+  // All Responsive Tables
+  $( ".table" ).wrap( "<div class='table-responsive'></div>" );
+
+  // For Header Responsive
+  $(".search_toggle").click(function(){
+    $(".search_area").toggleClass("search_open");
+    $("body").toggleClass("modal-open");
+    $(".responsive-overlay").toggleClass("open");
+  });
+
+  $(".userbar_toggle").click(function(){
+    $("#top-nav-bar").toggleClass("userbar_open");
+    $("body").toggleClass("modal-open");
+    $(".responsive-overlay").toggleClass("open");
+  });
+
+  $(".sidebar_toggle").click(function(){
+    $("#sidebar").toggleClass("sidebar_open");
+    $("body").toggleClass("modal-open");
+    $(".responsive-overlay").toggleClass("open");
+  });
+
+});

0c442eff1cc9e7a10dc6f6a88bee3aafd97b6484
Maheswaran <t.mahesmca@gmail.com>
2018-10-25 12:16:00 +0530

    three type push

--- app/controllers/spree/admin/products_controller.rb
+++ app/controllers/spree/admin/products_controller.rb
@@ -70,7 +70,7 @@ module Spree
 unless params[:variant].nil?
 params[:variant].each do |variant_params|
   logger.debug "@iamgesssssssssss #{variant_params["price"]}"
-  @optionValue = Spree::OptionValue.where(id:[variant_params["option_types_0"],variant_params["option_types_1"]])
+  @optionValue = Spree::OptionValue.where(id:[variant_params["option_types_0"],variant_params["option_types_1"],variant_params["option_types_2"]])
     logger.debug "Creating Variants #{@productObj.id}"
   @variantnewObj =  Spree::Variant.create!({:product_id => @productObj.id, :sku => "", :cost_price => variant_params["price"], :is_master => false, :option_values => @optionValue})
   @productObj.variants << @variantnewObj
